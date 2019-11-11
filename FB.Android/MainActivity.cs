887919fa46d2a1c8a7ca7716656341815511fb10
Timeworks Dev <office@timeworksdata.co.za>
2019-11-11 10:45:29 +0200

    Initial commit

---
+++ FB.Android/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.WindowsAzure.MobileServices;
+using System.IO;
+
+namespace FB.Droid
+{
+    [Activity(Label = "FB", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            //The following line added to support the XAML CollectionView
+            global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            CurrentPlatform.Init();
+
+            string filePath = "Famous.db3";
+            string locationPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string fullPath = Path.Combine(locationPath, filePath);
+            LoadApplication(new App(fullPath));
+
+           
+            //LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
